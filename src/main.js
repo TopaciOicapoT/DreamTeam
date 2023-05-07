eacb142dda6694f727c03848d583624a0b251284
Your NameSaty <escuelaps@gmail.com>
2023-05-07 04:10:57 +0200

    Añade primeros componentes en antdesing

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
 import router from './router/routes'
 import { createPinia } from "pinia"
-import { VueQueryPlugin } from "@tanstack/vue-query";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
-app.use(router).use(createPinia()).mount('#app')
-app.use(VueQueryPlugin)
- 
+app.use(router).use(createPinia()).use(Antd).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,64 @@
     <h1 class="title1">DREAM TEAM</h1>
     <div class="homeBox">
         <div class="box">
-            <h3>Clasificación motoGP</h3>
-            <ul class="list-group">
-                <!-- Una vez creada la base de datos con los usuarios apareceria aqui el usuario y la puntuación de su equipoo -->
-            <li class="list-group-item" >Mauricio Colmenero: {{ totalPoint }}</li>
-        </ul>
+          <h3 :style="{ margin: '16px 0' }">Clasificación motoGP</h3>
+          <a-list size="small" bordered :data-source="ridersList">
+            <template #renderItem="{ item }">
+              <a-list-item>{{ item.rider }}</a-list-item>
+              <CheckCircleFilled :style="{color: item.active ? 'green' :  'red'}" />
+            </template>
+            <!-- <template #header>
+              <div>Todos los pilotos</div>
+            </template>
+            <template #footer>
+              <div>Se acabaron los pilotos</div>
+            </template> -->
+          </a-list>
+            <!-- <h3>Clasificación motoGP</h3>
+            <a-list
+              itemLayout="horizontal"
+              :dataSource="ridersList"
+            >
+              <a-list-item slot="renderItem" slot-scope="item, index">
+                <a-list-item-meta                >
+                  <a slot="title">{{item.rider}}</a>
+                </a-list-item-meta>
+              </a-list-item>
+            </a-list> -->
+          <!-- <a-list item-layout="horizontal" :data-source="ridersList">
+            <a-list-item #renderItem="{ item }">
+              <a-list-item-meta
+                description="hola"
+              >
+                <template #title>
+                 <span>{{ item.rider }}</span>
+                 <check-circle-filled :style="{color: item.active ? 'green' : 'red'}"/>
+                </template>
+                <template #points>
+                 <span>{{ item.newPoints }}</span>
+                </template>
+                <template #avatar>
+                  <a-avatar src="https://joeschmoe.io/api/v1/random" />
+                </template>
+              </a-list-item-meta>
+            </a-list-item>
+          </a-list> -->
         </div>
         <div class="box">
             <h3>Clasificación moto 2</h3>
+            <ul class="list-group">
+        <li
+          class="list-group-item"
+          v-for="(rider, index) in ridersList"
+          :key="index"
+        >
+          {{ rider.rider }} {{ rider.position }}ª
+          <div v-if="rider.active" class="yes">
+            <span>Activo</span>
+          </div>
+          <div v-else class="no"><span>Inactivo</span> <br /></div>
+        </li>
+      </ul>
         </div>
         <div class="box">
             <h3>Clasificación moto 3</h3>
@@ -23,8 +73,9 @@
 import '../assets/styles.scss'
 import { onMounted, ref } from 'vue';
 import { useTeams } from "../stores/teams";
+import { CheckCircleFilled } from '@ant-design/icons-vue';
 const storeTeams = useTeams();
-
+const ridersList = storeTeams.riders;
 const userTeam = storeTeams.userTeam;
 const totalPoint = ref(0);
 const suma = () => {
@@ -45,15 +96,18 @@ onMounted(() => {
 }
 .homeBox{
   display: flex;
+  height: 100%;
   place-items: center;
   justify-content: space-around;
   padding: 1rem;
   border-right: 1px solid gray;
   border-left: 1px solid gray;
+  overflow: hidden;
   .box{
     padding: 1rem;
     border-right: 1px solid gray;
     border-left: 1px solid gray;
+
   }
 }
 </style>
