44ad4b697e84f0920d7804b87520b446265dbfff
Your NameSaty <escuelaps@gmail.com>
2023-05-10 19:03:24 +0200

    Añadido el endpoint de Teams

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,2 +1,3 @@
 export const SearchPexels = "https://api.pexels.com/v1/search";
 export const ridersMockup = "https://api.mockaroo.com/api/3ca4cf80?count=100&key=59f73b60";
+export const teamsMockup = "https://api.mockaroo.com/api/9794e140?count=20&key=59f73b60";
