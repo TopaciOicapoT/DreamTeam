af268eaf21e2d66b81b7690d1be3a19b504e4225
Your NameSaty <escuelaps@gmail.com>
2023-07-14 00:09:54 +0200

    Mock Theme Config

---
+++ themeConfig.js
@@ -0,0 +1,12 @@
+export const themeConfig = {
+    "primary-color": "#3F88C5",
+    "layout-header-background":"#011936",
+    "body-background": "#060019",
+    "link-color": "red",
+    "text-color": "#FFFFFF",
+    "menu-item": "#FFFFFF",
+    "component-background":"#060019",
+    "layout-body-background":"#060019"
+  }
+
+  export default themeConfig;
